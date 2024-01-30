8c3feab9db2c96dfef0b3dbaca8bab0ccb5fddc6
Spandan Mishra <47768055+itachi88@users.noreply.github.com>
2024-01-30 12:55:01 +0530

    socket boilerplate code

--- src/main/java/com/rakesh/codingbattle/controller/ContestController.java
+++ src/main/java/com/rakesh/codingbattle/controller/ContestController.java
@@ -4,21 +4,22 @@ import com.rakesh.codingbattle.controller.request.CreateContestRequest;
 import com.rakesh.codingbattle.controller.request.JoinContestRequest;
 import com.rakesh.codingbattle.controller.response.Contest;
 import com.rakesh.codingbattle.service.ContestService;
+import lombok.AllArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("v1/contest")
 public class ContestController {
 
 
     private ContestService contestService;
-
-    public ContestController(ContestService contestService) {
-        this.contestService = contestService;
-    }
 
     @PostMapping
     public Contest createContest(@RequestBody CreateContestRequest createContestRequest) {
@@ -32,6 +33,12 @@ public class ContestController {
 
         return contestService.joinContest(joinContestRequest);
 
+    }
+
+    @MessageMapping("/contest/{id}")
+    @SendTo("/cb-topic/{id}")
+    public String handleMessage(@DestinationVariable String id, String message) {
+        return "Success";
     }
 
 }
