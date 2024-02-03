a7c2755c48ecef7304db0bffd9c84d616dc3d5d7
RakeshRanjanDas <rakeshranjandas.bbsr93@gmail.com>
2024-02-03 18:39:41 +0530

    Fix deserialize error of SubmitEvent

--- src/main/java/com/rakesh/codingbattle/model/SubmitEvent.java
+++ src/main/java/com/rakesh/codingbattle/model/SubmitEvent.java
@@ -4,10 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
 public class SubmitEvent extends Event{
     private Long contestQuestionId;
+
+    public SubmitEvent() {
+        super();
+    }
 
 }
